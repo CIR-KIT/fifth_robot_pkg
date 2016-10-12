b89e98214cc801adbe859d05f3cf155a84056fb9
HajimeTokuyasu <p111026h@mail.kyutech.jp>
2016-10-12 21:15:27 +0900

    Add launch of tf

--- src/yaml_reader/src/yaml_reader.py
+++ src/yaml_reader/src/yaml_reader.py
@@ -13,15 +13,10 @@ waypoints = yaml.load(f)
 f.close()
 
 
-def checkImportance():
-    if waypoints[0]["inportance"] == 1:
-       print '%s is Passing point.' % waypoints[0]["name"]
-    if waypoints[1]["inportance"] == 2:
-       print '%s is Important Passing point.' % waypoints[1]["name"]
-    if waypoints[2]["inportance"] == 3:
-       print '%s is Dangerous point.' % waypoints[2]["name"]
-    if waypoints[3]["inportance"] == 4:
-       print '%s is destination.' % waypoints[3]["name"]
+def check(k, v):
+    print waypoints[k][v]
+
+
 
 def pubData():
     pub = rospy.Publisher('data', String, queue_size=10)
