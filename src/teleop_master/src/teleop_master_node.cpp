3b5357440c078a46a3970ca51a72d28769da7333
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-11-05 02:50:44 +0900

    Refactor teleop master

--- src/teleop_master/src/teleop_master_node.cpp
+++ src/teleop_master/src/teleop_master_node.cpp
@@ -3,23 +3,19 @@
 
 ros::Publisher pub;
 
-void broadcaster( const geometry_msgs::Twist::ConstPtr& msg ) {
+void broadcaster(const geometry_msgs::Twist::ConstPtr& msg) {
    pub.publish(msg);
 }
 
 int main(int argc, char** argv) {
    ros::init(argc, argv, "teleop_master_node");
    ros::NodeHandle n;
-
    pub = n.advertise<geometry_msgs::Twist>("dest", 1);
-
    ros::Subscriber sub = n.subscribe("source", 100000, broadcaster);
 
    ros::Rate loop(100);
-
    while (ros::ok()){
       ros::spinOnce();
-      
       loop.sleep();
    }
    return 0;
