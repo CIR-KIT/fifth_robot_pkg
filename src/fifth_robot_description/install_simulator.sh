552ae58c07c57f256a72f9bb6b21ee9949557a27
hideyuki0121 <satellite.craft@gmail.com>
2017-10-04 19:38:41 +0900

    Add installer

---
+++ src/fifth_robot_description/install_simulator.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get install ros-kinetic-ros-controller-manager
+sudo apt-get install ros-kinetic-gazebo-ros-control
+sudo apt-get install ros-kinetic-diff-drive-controller
+sudo apt-get install ros-kinetic-joint-state-controller
