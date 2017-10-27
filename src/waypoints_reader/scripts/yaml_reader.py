4eaaacb72f5408b49e2bea8522dcd1d5f0a4021a
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-10-27 21:31:40 +0900

    Add template of waypoint_reader

--- src/waypoints_reader/scripts/yaml_reader.py
+++ src/waypoints_reader/scripts/yaml_reader.py
@@ -4,21 +4,31 @@
 import yaml
 import rospy
 
-from std_msgs.msg import String
+from goal_sender_msgs.msg import GoalSequence
+from goal_sender_msgs.msg import Waypoint
 
-def pubData():
-    pub = rospy.Publisher('data', String, queue_size=10)
-    rospy.init_node('pubData', anonymous=True)
-    f = open(rospy.get_param('path', 'waypoints.yaml'), 'r')
+def read_yaml(path):
+    f = open(path, 'r')
     waypoints = yaml.load(f)
     f.close()
-    r = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        pub.publish(waypoints)
-        r.sleep()
+    return waypoints
+
+def pub_data():
+    pub = rospy.Publisher('goal_sequence', GoalSequence, queue_size=10)
+    rospy.init_node('yaml_reader', anonymous=True)
+    msg = GoalSequence()
+    for waypoint_data in read_yaml(rospy.get_param('~path', 'waypoints.yaml')):
+        waypoint = Waypoint(name = waypoint_data['name'],
+                            x = waypoint_data['x'],
+                            y = waypoint_data['y'],
+                            importance = waypoint_data['importance'],
+                            radius = waypoint_data['radius'],
+                            drag = waypoint_data['drag'])
+        msg.waypoints.append(waypoint)
+    pub.publish(msg)
 
 if __name__ == '__main__':
     try:
-        pubData()
+        pub_data()
     except rospy.ROSInterruptException:
         pass
