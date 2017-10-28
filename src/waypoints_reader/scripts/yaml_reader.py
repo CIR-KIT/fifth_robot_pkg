b1244fe396e2ed6acb72b3ccd151fbde24f78198
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-10-28 20:22:11 +0900

    Adjust yaml_reader for Waypoint type

--- src/waypoints_reader/scripts/yaml_reader.py
+++ src/waypoints_reader/scripts/yaml_reader.py
@@ -5,8 +5,8 @@ import yaml
 
 import rospy
 from goal_sender_msgs.srv import ApplyGoals
-from goal_sender_msgs.msg import GoalSequence
-from goal_sender_msgs.msg import Waypoint
+from goal_sender_msgs.msg import GoalSequence, Waypoint
+from geometry_msgs.msg import Point, Quaternion
 
 def read_yaml(path):
     f = open(path, 'r')
@@ -18,8 +18,9 @@ def get_waypoints():
     sequence = GoalSequence()
     for waypoint_data in read_yaml(rospy.get_param('~path', 'waypoints.yaml')):
         waypoint = Waypoint(name = waypoint_data.get('name', ""),
-                            x = waypoint_data['x'], # required
-                            y = waypoint_data['y'], # required
+                            position = Point(x = waypoint_data['x'],
+                                             y = waypoint_data['y']),
+                            orientation = Quaternion(w = 1), # not havn't rotation
                             radius = waypoint_data['radius'], # required
                             importance = waypoint_data.get('importance', 0),
                             drag = waypoint_data.get('drag', 0))
