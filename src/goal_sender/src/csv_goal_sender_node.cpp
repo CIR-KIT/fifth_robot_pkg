9107395c3f96e60eac7f8cf2367534c841e87b28
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-10-28 10:13:28 +0900

    Rename goal_sender to csv_goal_sender

--- src/goal_sender/src/csv_goal_sender_node.cpp
+++ src/goal_sender/src/csv_goal_sender_node.cpp
@@ -1,16 +1,14 @@
-#include"ros/ros.h"
-#include"actionlib/client/simple_action_client.h"
-#include"geometry_msgs/Pose.h"
-#include"move_base_msgs/MoveBaseAction.h"
-#include"move_base_msgs/MoveBaseGoal.h"
-#include"tf/transform_listener.h"
+#include "ros/ros.h"
+#include "actionlib/client/simple_action_client.h"
+#include "geometry_msgs/Pose.h"
+#include "move_base_msgs/MoveBaseAction.h"
+#include "move_base_msgs/MoveBaseGoal.h"
+#include "tf/transform_listener.h"
 
-#include<stdexcept>
-#include<string>
-#include<utility>
-#include<vector>
-#include<fstream> // for csv
-#include<sstream> // ditto
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
 
 geometry_msgs::Pose getFramePose(const std::string&, const std::string&);
 double calcDistance(const geometry_msgs::Pose&, const geometry_msgs::Pose&);
