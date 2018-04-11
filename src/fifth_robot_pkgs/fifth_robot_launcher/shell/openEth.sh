1346dcdf75f275019fce4e5087559ea951903e32
tanacchi <q111026d@mail.kyutech.jp>
2018-04-11 19:15:02 +0900

    [WIP] Add launcher for connection
    launch からの呼び出しだと if の部分が上手く動作しない様子

--- src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
+++ src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ $# -lt 1 ]; then
-  echo "AutoDetect-"
-  target=` ifconfig | grep enp | cut -d' ' -f1 `
-  echo $target
-else 
-  target=$1
-fi
+# if [ $# -lt 1 ]; then
+#   echo "AutoDetect-"
+#   target=` ifconfig | grep enp | cut -d' ' -f1 `
+#   echo $target
+# else 
+#   target=$1
+# fi
 
 
 ip_address="192.168.0.15"
 
-sudo ifconfig $target $ip_address
+sudo ifconfig enp0s31f6 $ip_address
 sudo route add default gw 192.168.0.1
