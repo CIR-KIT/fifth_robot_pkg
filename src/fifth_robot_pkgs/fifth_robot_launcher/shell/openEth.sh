17bedd4583914bbbee14ad7c144c91c21d06cf86
tanacchi <q111026d@mail.kyutech.jp>
2018-04-12 14:29:29 +0900

    Activate code for auto connection

--- src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
+++ src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# if [ $# -lt 1 ]; then
-#   echo "AutoDetect-"
-#   target=` ifconfig | grep enp | cut -d' ' -f1 `
-#   echo $target
-# else 
-#   target=$1
-# fi
+if [ $# -lt 1 ]; then
+  echo "AutoDetect-"
+  target=` ifconfig | grep enp | cut -d' ' -f1 `
+  echo $target
+else 
+  target=$1
+fi
 
 
 ip_address="192.168.0.15"
