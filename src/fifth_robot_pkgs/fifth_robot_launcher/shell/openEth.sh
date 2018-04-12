86bd4222a78f25af184ae42ae719cce44fe2ca42
tanacchi <q111026d@mail.kyutech.jp>
2018-04-12 17:40:08 +0900

    Modify to use device-name from command-line

--- src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
+++ src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
@@ -4,12 +4,13 @@ if [ $# -lt 1 ]; then
   echo "AutoDetect-"
   target=` ifconfig | grep enp | cut -d' ' -f1 `
   echo $target
-else 
+else
+  echo "Device \"$1\" was selected-"
   target=$1
+  echo $target
 fi
-
 
 ip_address="192.168.0.15"
 
-sudo ifconfig enp0s31f6 $ip_address
+sudo ifconfig $target $ip_address
 sudo route add default gw 192.168.0.1
