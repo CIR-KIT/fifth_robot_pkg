eafb3d570fbe19020ac12dcda18ad86f5d2bdc9f
tanacchi <q111026d@mail.kyutech.jp>
2018-04-19 23:48:35 +0900

    Refactor shell files for connection

--- src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
+++ src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
@@ -2,15 +2,14 @@
 
 if [ $# -lt 1 ]; then
   echo "AutoDetect-"
-  target=` ifconfig | grep enp | cut -d' ' -f1 `
-  echo $target
+  target_device=` ifconfig | grep enp | cut -d' ' -f1 `
 else
-  echo "Device \"$1\" was selected-"
-  target=$1
-  echo $target
+  echo "Device \"$target_device\" was selected-"
+  target_device=$1
 fi
 
 ip_address="192.168.0.15"
 
-sudo ifconfig $target $ip_address
+sudo ifconfig $target_device $ip_address
 sudo route add default gw 192.168.0.1
+echo -e "\e[32;1mSucceeded in Ethernet connection to $target_device\e[m"
--- src/fifth_robot_pkgs/fifth_robot_launcher/shell/openUSB.sh
+++ src/fifth_robot_pkgs/fifth_robot_launcher/shell/openUSB.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo chmod 777 /dev/serial/by-id/usb-T-frog_project_T-frog_Driver-if00
-echo "Succeed in connecting to yp-spur"
+echo -e "\e[32;1mSucceeded in USB connection to yp-spur\e[m"
