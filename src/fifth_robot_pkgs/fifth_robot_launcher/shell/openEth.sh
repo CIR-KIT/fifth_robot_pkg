4f019bbec20b30ae6e203a419650cb879f9dbc14
tanacchi <q111026d@mail.kyutech.jp>
2018-04-20 11:31:25 +0900

    Update auto-delect message

--- src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
+++ src/fifth_robot_pkgs/fifth_robot_launcher/shell/openEth.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ $# -lt 1 ]; then
-  echo "AutoDetect-"
   target_device=` ifconfig | grep enp | cut -d' ' -f1 `
+  echo "Device \"$target_device\" was DETECTED automatically."
 else
-  echo "Device \"$target_device\" was selected-"
   target_device=$1
+  echo "Device \"$target_device\" was SELECTED with argument."
 fi
 
 ip_address="192.168.0.15"
