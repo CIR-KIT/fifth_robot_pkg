f560bdb5c1cd383458027ddee622cfe8218c20b4
tanacchi <q111026d@mail.kyutech.jp>
2018-07-26 19:47:33 +0900

    Refactor shellscript to install required packages

--- src/fifth_robot_pkgs/fifth_robot_description/install_simulator.sh
+++ src/fifth_robot_pkgs/fifth_robot_description/install_simulator.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install ros-kinetic-controller-manager ros-kinetic-gazebo-ros-control ros-kinetic-diff-drive-controller ros-kinetic-joint-state-controller ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-teleop-twist-keyboard ros-kinetic-velodyne-gazebo-plugins
+sudo apt-get install\
+     ros-kinetic-controller-manager\
+     ros-kinetic-gazebo-ros-control\
+     ros-kinetic-diff-drive-controller\
+     ros-kinetic-joint-state-controller\
+     ros-kinetic-ros-control\
+     ros-kinetic-ros-controllers\
+     ros-kinetic-teleop-twist-keyboard\
+     ros-kinetic-velodyne-gazebo-plugins
